86ae2ce2775d89eaece6c0cae293ea071fc34726
Milan <milanstoj94@gmail.com>
2020-09-23 13:52:21 -0500

    init models and properties

---
+++ Flow/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flow.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Plant")]
+        public int PlantID { get; set; }
+        public virtual Plant Plant { get; set; }
+    }
+}
---
+++ Flow/Models/Workstation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flow.Models
+{
+    public class Workstation
+    {
+        [Key]
+        public int ID { get; set; }
+
+
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+        public virtual Department Department { get; set; }
+
+    }
+}
