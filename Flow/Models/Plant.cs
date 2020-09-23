c24537168f15c3c2c5599285d6a14a32a214f965
Milan <milanstoj94@gmail.com>
2020-09-23 14:21:07 -0500

    update plant & workstation model props

--- Flow/Models/Plant.cs
+++ Flow/Models/Plant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,12 @@ namespace Flow.Models
 {
     public class Plant
     {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
 
     }
 }
